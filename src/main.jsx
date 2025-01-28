497a16b7917e189f427a7cf4242d3a3f8f271021
polezibreno <163679561+polezibreno@users.noreply.github.com>
2025-01-28 12:20:20 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 //import Cadastro from './pages/login'
-import Login from './pages/Cadastro'
+//import Login from './pages/Cadastro'
 import Cadastro from './pages/Cadastro'
 
 createRoot(document.getElementById('root')).render(
